434f80b0dcc4346e552723f5760d34fa5dcade2a
Arpita Patel <gagarp@gmail.com>
2023-05-23 11:57:41 -0500

    Updated github URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-           editUrl:'https://github.com/AlabamaWaterInstitute/alabamawaterinstitute_website/edit/main/docs/',
+           editUrl:'https://github.com/AlabamaWaterInstitute/alabamawaterinstitute_website/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

c7eef98f3ca256050a16442aede28a8fadca4beb
Arpita Patel <gagarp@gmail.com>
2023-10-27 13:48:12 -0500

    Added logos for social media

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,46 +1,46 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'CIROH - Cooperative Institute for Research to Operations in Hydrology',
-  staticDirectories: ['static','img'],
-  tagline: 'CIROH Docs - Portal for the development activities of CIROH',
-  url: 'http://ciroh.org',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/ciroh_logo.png',
+  title:
+    "CIROH DocuHub",
+  staticDirectories: ["static", "img"],
+  tagline: "Cooperative Institute for Research to Operations in Hydrology",
+  url: "http://ciroh.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/ciroh_logo.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'CIROH-UA', // Usually your GitHub org/user name.
-  projectName: 'CIROH-UA', // Usually your repo name.
+  organizationName: "CIROH-UA", // Usually your GitHub org/user name.
+  projectName: "CIROH-UA", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
-
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-           editUrl:'https://github.com/CIROH-UA/ciroh-ua_website/edit/main/',
+          editUrl: "https://github.com/CIROH-UA/ciroh-ua_website/edit/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -49,85 +49,105 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      
+      stylesheets: [
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css",
+        // ... other stylesheets
+      ],
       navbar: {
-        title: 'CIROH Docs',
+        title: "CIROH Docs",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/ciroh_logo.png',          
+          alt: "CIROH Logo",
+          src: "img/ciroh_logo.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'products/products-intro',
-            position: 'left',
-            label: 'Products',
-          }, 
-          {
-            type: 'doc',
-            docId: 'services/services',
-            position: 'left',
-            label: 'Cloud Services',
+            type: "doc",
+            docId: "products/products-intro",
+            position: "left",
+            label: "Products",
           },
           {
-            type: 'doc',
-            docId: 'education/education',
-            position: 'left',
-            label: 'Education',
+            type: "doc",
+            docId: "services/services",
+            position: "left",
+            label: "Cloud Services",
           },
           {
-            href: 'https://ciroh.org',
-            label: 'ciroh.org',
-            position: 'left',
-          }, 
-          {
-            href: '/about',
-            label: 'About us',
-            position: 'right',
+            type: "doc",
+            docId: "education/education",
+            position: "left",
+            label: "Education",
           },
           {
-            href: '/Contribute',
-            label: 'How to contribute',
-            position: 'right',
+            href: "https://ciroh.org",
+            label: "ciroh.org",
+            position: "left",
           },
           {
-            href: 'https://github.com/CIROH-UA',
-            label: 'GitHub',
-            position: 'right',
+            href: "/about",
+            label: "About us",
+            position: "right",
           },
+          {
+            href: "/Contribute",
+            label: "How to contribute",
+            position: "right",
+          },
+          // {
+          //   href: 'https://github.com/CIROH-UA',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'About CIROH',
-                to: '/about',
+                label: "About CIROH",
+                to: "/about",
+              },
+              {
+                label: "Visit ciroh.org",
+                href: "https://ciroh.org",
               },
             ],
           },
           {
-            title: 'Community',
-            items: [              
+            title: "Follow us on",
+            items: [
               {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/uaciroh/',
+                html: `
+                <a href="https://ciroh.org" target="_blank" rel="noreferrer noopener" aria-label="Visit CIROH" style="margin-left:-15px">
+                <img src="https://static.vecteezy.com/system/resources/previews/018/930/572/non_2x/youtube-logo-youtube-icon-transparent-free-png.png" alt="CIROH on YouTube" width="70" height="60" />
+              </a>
+              <a href="https://ciroh.org" target="_blank" rel="noreferrer noopener" aria-label="Visit CIROH"  style="margin-left:-15px">
+                <img src="https://static.vecteezy.com/system/resources/previews/018/930/587/non_2x/linkedin-logo-linkedin-icon-transparent-free-png.png" alt="CIROH on LinkedIn" width="70" height="60" />
+              </a>
+              <a href="https://ciroh.org" target="_blank" rel="noreferrer noopener" aria-label="Visit CIROH"  style="margin-left:-25px">
+                <img src="https://static.vecteezy.com/system/resources/previews/018/930/702/original/facebook-logo-facebook-icon-transparent-free-png.png" alt="CIROH on Facebook" width="70" height="60" />
+              </a> 
+                  
+                `,
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/UA_CIROH',
+                html: `
+                <a href="https://ciroh.org" target="_blank" rel="noreferrer noopener" aria-label="Visit CIROH"  style="margin-left:0px;">
+                <img src="https://static.vecteezy.com/system/resources/previews/023/986/555/original/instagram-logo-instagram-logo-transparent-instagram-icon-transparent-free-free-png.png" alt="CIROH on Instagram" width="45" height="45" />
+              </a>
+              <a href="https://ciroh.org" target="_blank" rel="noreferrer noopener" aria-label="Visit CIROH"  style="margin-left:5px;">
+                <img src="https://seeklogo.com/images/T/twitter-x-logo-0339F999CF-seeklogo.com.png?v=638264860180000000" alt="CIROH on X" width="40" height="40" />
+              </a>
+              <a href="https://ciroh.org" target="_blank" rel="noreferrer noopener" aria-label="Visit CIROH"  style="margin-left:5px;">
+                <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt="CIROH on GitHUb" width="40" height="40" />
+              </a>
+                
+                `,
               },
-            ],
-          },
-          {
-            title: 'More',
-            items: [              
-              {
-                label: 'GitHub',
-                href: 'https://github.com/CIROH-UA',
-              }
             ],
           },
         ],

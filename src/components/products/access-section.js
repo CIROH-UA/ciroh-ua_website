082b8928ce0559d064d365f63c8bdbda54996e19
Sheargrub <sheargrub@gmail.com>
2025-04-02 11:45:47 -0500

    Final updates
    Add image to CHMF landing page
    Rework "NGIAB at a glance" to use tabs + emoji check marks
    Remove status table for now (to be reintroduced in a new PR)

---
+++ src/components/products/access-section.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useCollapse } from "react-collapsed";
+
+const AccessSection = ({ display }) => {
+    return (
+        <section style={{'display': display ? 'block' : 'none'}}>
+            <table>
+                <thead>
+                <tr>
+                    <th>Access method</th>
+                    <th><a href="/docs/products/Community Hydrologic Modeling Framework/ngiabpreprocessor/">Data Preprocess</a></th>
+                    <th>
+                    <p style={{'margin':'0'}}>NGIAB Implementation</p>
+                    <p style={{'margin':'0'}}>(<a href="/docs/products/Community Hydrologic Modeling Framework/nextgeninaboxDocker/">Cloud</a>, <a href="/docs/products/Community Hydrologic Modeling Framework/nextgeninaboxSingularity/">HPC</a>)</p>
+                    </th>
+                    <th><a href="/docs/products/Evaluation Tools/rtiteehr/">TEEHR Evaluation</a></th>
+                    <th><a href="/docs/products/Visualization and Analysis Tools/tethys-cses/">Data Visualizer</a></th>
+                    <th><a href="/docs/products/Community Hydrologic Modeling Framework/nextgenDatastream/">DataStreamCLI</a></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Docker (cloud)</td>
+                    <td>-</td>
+                    <td>✅</td>
+                    <td>✅</td>
+                    <td>✅</td>
+                    <td>✅</td>
+                </tr>
+                <tr>
+                    <td>Python Package (pip)</td>
+                    <td>✅</td>
+                    <td>✅</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Web Interface</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Notebook (ipynb)</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Singularity (HPC)</td>
+                    <td>-</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                </tbody>
+            </table>
+        </section>
+    );
+}
+
+export default AccessSection;
---
+++ src/components/products/capabilities-section.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { useCollapse } from "react-collapsed";
+
+const CapabilitiesSection = ({ display }) => {
+    return (
+        <section style={{'display': display ? 'block' : 'none'}}>
+            <table>
+                <thead>
+                <tr>
+                    <th>Capability</th>
+                    <th><a href="/docs/products/Community Hydrologic Modeling Framework/ngiabpreprocessor/">Data Preprocess</a></th>
+                    <th><a href="/docs/products/Evaluation Tools/rtiteehr/">TEEHR Evaluation</a></th>
+                    <th><a href="/docs/products/Visualization and Analysis Tools/tethys-cses/">Data Visualizer</a></th>
+                    <th><a href="/docs/products/Community Hydrologic Modeling Framework/nextgenDatastream/">DataStreamCLI</a></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>GUI</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>✅</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Hydrofabric Subsetting</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>✅ (view only)</td>
+                    <td>✅🔨</td>
+                </tr>
+                <tr>
+                    <td>NetCDF Forcing Processing</td>
+                    <td>-</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>✅🔨</td>
+                </tr>
+                <tr>
+                    <td>Zarr Forcing Processing</td>
+                    <td>✅</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Forcing Metadata Generation</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅🔨</td>
+                </tr>
+                <tr>
+                    <td>NextGen BMI Configuration File Generation</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅🔨</td>
+                </tr>
+                <tr>
+                    <td>Directory and File Format Validation</td>
+                    <td>🔨</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅🔨</td>
+                </tr>
+                <tr>
+                    <td>NextGen Execution via NGIAB</td>
+                    <td>✅</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅</td>
+                </tr>
+                <tr>
+                    <td>Execution Metadata Generation</td>
+                    <td class='checkmark-color'>🔨</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>✅🔨</td>
+                </tr>
+                <tr>
+                    <td>Calibration</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>Evaluation</td>
+                    <td>-</td>
+                    <td>✅🔨</td>
+                    <td>🔨 (displays TEEHR results)</td>
+                    <td>✅</td>
+                </tr>
+                <tr>
+                    <td>Visualization</td>
+                    <td>-</td>
+                    <td>🔨 (metrics visualization)</td>
+                    <td>✅</td>
+                    <td>-</td>
+                </tr>
+                </tbody>
+            </table>
+        </section>
+    );
+}
+
+export default CapabilitiesSection;

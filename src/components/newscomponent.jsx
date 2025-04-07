98302ddc34b34ab3c88d6dc8487a7f1ebbc74bc1
Sheargrub <sheargrub@gmail.com>
2025-04-07 13:21:56 -0500

    Fix image urls in news and community impact

--- src/components/newscomponent.jsx
+++ src/components/newscomponent.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import themes from "./themes.module.css";
 import ReactMarkdown from 'react-markdown';
 import Link from '@docusaurus/Link'
+import useBaseUrl from '@docusaurus/useBaseUrl'
 
 const NewsComponent = ({ data, isLatest }) => {
   const [isExpanded, setIsExpanded] = useState(isLatest); // Expand only the latest
@@ -25,21 +26,21 @@ const NewsComponent = ({ data, isLatest }) => {
 
       {isExpanded && 
         <div style={{'display':'flex','flexDirection':'row','justifyContent':'center','alignItems':'center'}}>
-          <img class={themes.darkImage} alt="CIROH logo" src="img/cirohlogo-white.png" style={{'alignSelf':'center','height':'145px','width':'145px','margin':'2rem'}}></img>
-          <img class={themes.lightImage} alt="CIROH logo" src="img/cirohlogo-transparent.png" style={{'alignSelf':'center','height':'145px','width':'145px','margin':'2rem'}}></img>
+          <img class={themes.darkImage} alt="CIROH logo" src={useBaseUrl("img/cirohlogo-white.png")} style={{'alignSelf':'center','height':'145px','width':'145px','margin':'2rem'}}></img>
+          <img class={themes.lightImage} alt="CIROH logo" src={useBaseUrl("img/cirohlogo-transparent.png")} style={{'alignSelf':'center','height':'145px','width':'145px','margin':'2rem'}}></img>
           <div style={{'display':'flex','flexDirection':'column','justifyContent':'center','alignItems':'center'}}>
             <div style={{'display':'flex','flexDirection':'row','margin':'0','padding':'0'}}>
-              <img class={themes.darkImage} alt="Wave graphic" src="img/news-sideline-dark.png" style={{'alignSelf':'center','height':'2rem'}}></img>
-              <img class={themes.lightImage} alt="Wave graphic" src="img/news-sideline-light.png" style={{'alignSelf':'center','height':'2rem'}}></img>
+              <img class={themes.darkImage} alt="Wave graphic" src={useBaseUrl("img/news-sideline-dark.png")} style={{'alignSelf':'center','height':'2rem'}}></img>
+              <img class={themes.lightImage} alt="Wave graphic" src={useBaseUrl("img/news-sideline-light.png")} style={{'alignSelf':'center','height':'2rem'}}></img>
               <div style={{'width':'2rem'}} />
               <p style={{'fontSize':'2rem','margin':'0'}}>News</p>
               <div style={{'width':'2rem'}} />
-              <img class={themes.darkImage} alt="Wave graphic" src="img/news-sideline-dark.png" style={{'alignSelf':'center','height':'2rem'}}></img>
-              <img class={themes.lightImage} alt="Wave graphic" src="img/news-sideline-light.png" style={{'alignSelf':'center','height':'2rem'}}></img>
+              <img class={themes.darkImage} alt="Wave graphic" src={useBaseUrl("img/news-sideline-dark.png")} style={{'alignSelf':'center','height':'2rem'}}></img>
+              <img class={themes.lightImage} alt="Wave graphic" src={useBaseUrl("img/news-sideline-light.png")} style={{'alignSelf':'center','height':'2rem'}}></img>
             </div>
             <p style={{'fontSize':'3rem','margin':'0','textAlign':'center'}}><b>{data.date}s</b></p>
-            <img class={themes.darkImage} alt="Wave graphic" src="img/news-underlines-dark.png" style={{'alignSelf':'center','height':'3.5rem'}}></img>
-            <img class={themes.lightImage} alt="Wave graphic" src="img/news-underlines-light.png" style={{'alignSelf':'center','height':'3.5rem'}}></img>
+            <img class={themes.darkImage} alt="Wave graphic" src={useBaseUrl("img/news-underlines-dark.png")} style={{'alignSelf':'center','height':'3.5rem'}}></img>
+            <img class={themes.lightImage} alt="Wave graphic" src={useBaseUrl("img/news-underlines-light.png")} style={{'alignSelf':'center','height':'3.5rem'}}></img>
           </div>
         </div>
       }

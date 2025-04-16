aa535a391e1f427a7ad7a85835c210258a141611
Sheargrub <69406976+Sheargrub@users.noreply.github.com>
2025-04-16 14:37:13 -0500

    Fix relative image paths

--- src/components/HomepageFeatures/testimonial.js
+++ src/components/HomepageFeatures/testimonial.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import './testimonial.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function ResearcherTestimonials() {
     const testimonials = [
@@ -8,21 +9,21 @@ export default function ResearcherTestimonials() {
             initial: "QL",
             quote: "I frequently use DocuHub to access documentation and best practices for the computing infrastructure I use for my research",
             date: "April 2025",
-            image: "/img/contributor1.png"
+            image: useBaseUrl("/img/contributor1.png")
         },
         {
             name: "Patrick Clemins",
             initial: "PC",
             quote: "CIROH's DocuHub allows us at the University of Vermont's NETWA (Northeast Evaluation Testbed for Water Resources Applications) to effectively engage with our community by providing a listing of our services, documentation for how to use them, and an introductory \"How to Get Started \" page.  It also allows us to share our progress and stay engaged with the broader CIROH community so that we can grow our impact as others discover and use our products and services.",
             date: "April 2025",
-            image: "/img/contributor2.png"
+            image: useBaseUrl("/img/contributor2.png")
         },
         {
             name: "Supath Dhital",
             initial: "SD",
             quote: "DocuHub has transformed my research process. I discovered several hydrological frameworks I wasn't previously aware of when searching for specific tools. The platform not only helps me learn from others' work, but also provides a professional space to document and publish my own research findings. When colleagues ask for my work, sharing a DocuHub link adds credibility and enhances collaboration. It's become an essential tool in my research workflow. ",
             date: "April 2025",
-            image: "/img/contributor3.png"
+            image: useBaseUrl("/img/contributor3.png")
         },
         {
             name: "Gio",
